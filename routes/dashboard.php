7e7569f649441a8d48c0f46fdcd3ecbf8af5efcc
Hridoy Roy <hridayroy05@gmail.com>
2022-04-05 18:47:05 +0600

    F update

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\frontend\DashboardController;
+use App\Http\Controllers\Frontend\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['auth','verified']], function () {
